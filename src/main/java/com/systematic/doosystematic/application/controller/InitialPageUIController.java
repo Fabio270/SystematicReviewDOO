d455858e115c1bc71b83a8ad85c0e33b7c23fd55
vitor-aguiarzz <galaxiustraveler@outlook.com>
2023-12-12 23:43:17 -0300

    adiciona window loader e reestrutura pastas

---
+++ src/main/java/com/systematic/doosystematic/application/controller/InitialPageUIController.java
@@ -0,0 +1,31 @@
+package com.systematic.doosystematic.application.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class InitialPageUIController {
+    @FXML
+    private ImageView imgLogo;
+
+    @FXML
+    private Button btnStart;
+
+    @FXML
+    private ImageView imgStart;
+
+    @FXML
+    private Button btnSaved1;
+
+    @FXML
+    public void initialize() {
+//        Image image1 = new Image("file:images/logo.png");
+//        Image image2 = new Image("file:images/play-button.png");
+//        imgLogo.setImage(image1);
+//        imgStart.setImage(image2);
+        System.out.println(imgLogo.getImage().getUrl());
+    }
+
+
+}
